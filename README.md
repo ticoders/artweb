2cbc48f9a287b4f4ba1affea614e94e8b79e1572
tidiong <62657965+tidiong@users.noreply.github.com>
2024-07-26 21:18:59 -0500

    Arctic Api  Artwork / Exhibition with basic HTML, CSS, and JavaScript files

---
+++ README.md
@@ -0,0 +1,32 @@
+# ARTIC  
+
+## Project Title
+**ARTIC Art Project Using API**
+
+## Description
+This project uses the ARTIC API to fetch and display information about artworks and exhibitions from the Art Institute of Chicago. It includes:
+- A photo of the artwork
+- A brief description of the artwork
+- The title of the artwork
+- Exhibition names
+
+## Usage
+- **Artworks Section**: Displays images and other information about the artworks.
+- **Exibition Section**: Displays the names and dates of birth of some of the exhibitions.
+
+Navigate between the pages by clicking either "Artworks" or "Exhibitions" in the navigation bar.
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- ARTIC API
+
+## Features
+- Displays artworks with images, titles, descriptions
+- Displays various exibitions
+ 
+ 
+
+
+
---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetchArtworks();
+    fetchExhibitions();
+});
+
+function fetchArtworks() {
+    const content = document.getElementById('content');
+    content.innerHTML = ''; // Clear content for artworks
+    fetch('https://api.artic.edu/api/v1/artworks')
+        .then(response => response.json())
+        .then(data => displayArtworks(data.data))
+        .catch(error => console.error(error));
+}
+
+function fetchExhibitions() {
+    const content = document.getElementById('content');
+    content.innerHTML = '';  
+    fetch('https://api.artic.edu/api/v1/exhibitions')
+        .then(response => response.json())
+        .then(data => displayExhibitions(data.data))
+        .catch(error => console.error(error));
+}
+
+function displayArtworks(artworks) {
+    const content = document.getElementById('content');
+    artworks.forEach(artwork => {
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.innerHTML = 
+           `<img src="https://www.artic.edu/iiif/2/${artwork.image_id}/full/400,/0/default.jpg" alt="${artwork.title}">
+            <h3>${artwork.title}</h3>
+            <p>${artwork.artist_display}</p>
+            <p>${artwork.thumbnail.alt_text}</p>`;
+             
+        
+        content.appendChild(box);
+    });
+}
+
+function displayExhibitions(exhibitions) {
+    const content = document.getElementById('content');
+    exhibitions.forEach(exhibition => {
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.innerHTML = 
+           `<h3>${exhibition.title}</h3>`;
+        content.appendChild(box);
+    });
+}
